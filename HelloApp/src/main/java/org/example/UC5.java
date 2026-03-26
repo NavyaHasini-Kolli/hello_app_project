b10cfb46ecd429c3f3c060a4eab0c226ec1172ef
Navya <navyahasinikolli@gmail.com>
2026-03-26 15:47:58 +0530

    UC5 added

---
+++ HelloApp/src/main/java/org/example/UC5.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class UC5 {
+
+        public static void main(String[] args) {
+
+            StringBuilder namesBuilder = new StringBuilder();
+
+            // Check if no arguments are provided
+            if (args.length == 0) {
+                System.out.println("Hello, World!");
+                return;
+            }
+
+            // Enhanced for loop to iterate through names
+            for (String name : args) {
+                if (namesBuilder.length() > 0) {
+                    namesBuilder.append(", ");
+                }
+                namesBuilder.append(name);
+            }
+
+            // Final message
+            System.out.println("Hello, " + namesBuilder.toString() + "!");
+        }
+    }
+
+
